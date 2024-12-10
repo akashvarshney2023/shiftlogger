e0aebad4c41cfd2c659d871a02e7208dda44b8ee
Akash Varshney <akashvarshney2023@outlook.com>
2024-12-10 21:23:06 +0000

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      class="px-3"
+      density="compact"
+      flat
+    >
+      <v-spacer></v-spacer>
+
+      <v-tabs
+        color="grey-darken-2"
+        centered
+      >
+        <v-tab
+          v-for="link in links"
+          :key="link"
+          :text="link"
+        ></v-tab>
+      </v-tabs>
+      <v-spacer></v-spacer>
+
+      <v-avatar
+        class="hidden-sm-and-down"
+        color="grey-darken-1"
+        size="32"
+      ></v-avatar>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <Home></Home>
+    </v-main>
+  </v-app>
+</template>
+
+
+
+<script setup lang="ts">
+   const links = [
+    'Home',
+    'Report',   
+  ]
+</script>
